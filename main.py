e25f4ccaab203882804597a333bf92bbb1fc5640
vdelguercio <vdelguercio@gmail.com>
2025-09-18 17:38:10 -0400

    built a book search and print function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(book_path):
+    with open(book_path) as f:
+        return f.read()
+
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    print(book_text)
+
+main ()
